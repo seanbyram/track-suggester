9c3177be43ffee77ddb0eeda6876f6eae39af50d
Sean Byram <seanbyram@icloud.com>
2019-01-18 11:28:55 -0800

    add logic for form submission event  , update survey questions

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+
+// backend logic
+
+
+
+
+
+
+// user interface logic
+
+$(document).ready(function() {
+
+
+$("#survey").submit(function(event){
+
+  var select1 = $("#select1").val();
+  var select2 = $("#select2").val();
+  var select3 = $("#select3").val();
+  var select4 = $("#select4").val();
+  var select5 = $("#select5").val();
+
+  console.log(select1);
+
+
+  event.preventDefault();
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
