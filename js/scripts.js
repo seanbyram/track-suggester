a056bd87de2d0035b312ba6331951765dcd31698
Sean Byram <seanbyram@icloud.com>
2019-01-18 15:02:55 -0800

    finish survey logic and question flow

--- js/scripts.js
+++ js/scripts.js
@@ -10,41 +10,46 @@
 
 $(document).ready(function() {
 
+  $("#survey").submit(function(event){
 
-$("#survey").submit(function(event){
+    var select1 = $("#select1").val();
+    var select2 = $("#select2").val();
+    var select3 = $("#select3").val();
+    var select4 = $("#select4").val();
+    var select5 = $("#select5").val();
 
-  var select1 = $("#select1").val();
-  var select2 = $("#select2").val();
-  var select3 = $("#select3").val();
-  var select4 = $("#select4").val();
-  var select5 = $("#select5").val();
+    var allSelects = [select1, select2, select3, select4, select5];
+    console.log(allSelects);
 
-  console.log(select1);
-
-
-  event.preventDefault();
-
-});
-
-
-
-
-
-
-
-
-
-
-
+    if (select1 === "No") {
+        $("#result").text(" Start with our Intro to Programming Track")
+    } else if (select2 === "No") {
+        $("#result").text(" Try our Javascript Track")
+    } else if (select3 === "Yes") {
+        $("#result").text(" Try our CSS/Design Track")
+    } else if (select4 === "Yes") {
+        $("#result").text(" Try our C# Track")
+    } else if (select5 === "Yes") {
+        $("#result").text(" Try our Java Track")
+    } else {
+      $("#result").text(" Try our PHP or Ruby/Rails Track")
+    }
 
 
+    event.preventDefault();
+
+  });
+
+  function answerTree() {
+
+    if (select1 === "No. What is programming?") {
+      $("#result").text("Start with our Intro to Programming Track")
+    } else if (select1 === "Yes") {
+
+    }
+
+  }
 
 
 
-
-
-
-
-
-
-});
+  });
